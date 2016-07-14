801f5c711ebb16b93421cc7eca196f3bc5a0d309
chrisforrette <chris@chrisforrette.com>
2016-07-14 11:35:48 -0700

    Updating some Ruby stuff, adding some bits for Google Cloud, adding autoloader for .nvmrc

--- ruby/install.sh
+++ ruby/install.sh
@@ -4,6 +4,13 @@ if test ! $(which rbenv)
 then
   echo "  Installing rbenv for you."
   brew install rbenv > /tmp/rbenv-install.log
+  
+  # Install rbenv-aliases: https://github.com/tpope/rbenv-aliases
+  # Auto detects ruby version from .ruby-version and Gemfile files
+
+  mkdir -p "$(rbenv root)/plugins"
+  git clone git://github.com/tpope/rbenv-aliases.git "$(rbenv root)/plugins/rbenv-aliases"
+  rbenv alias --auto
 fi
 
 if test ! $(which ruby-build)
