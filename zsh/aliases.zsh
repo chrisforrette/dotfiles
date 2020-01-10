84bfc8e253b6a348d0d7a6ed5b1b5bf88e0237bc
chrisforrette <chris@chrisforrette.com>
2020-01-09 22:47:14 -0800

    Merging from remote

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,11 @@
 alias reload!='. ~/.zshrc'
+
+# Stop trying to correct things I use
+
+alias gulp='nocorrect gulp'
+alias serverless='nocorrect serverless'
+alias tsc='nocorrect tsc'
+
+# Docker Compose shortcut
+
+alias dcm='docker-compose'
