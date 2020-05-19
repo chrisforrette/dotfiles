6c3f7a6f593bb4a5ed39e277088855086133706d
chrisforrette <chris@chrisforrette.com>
2020-05-18 21:49:19 -0700

    Removing all Python 2 and virtualenv stuff

--- python/install.sh
+++ python/install.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-brew install python python@2;
-
-# Python 2 packages
-
-sudo pip2 install virtualenvwrapper;
+brew install python;
--- python/path.zsh
+++ python/path.zsh
@@ -1,20 +1,3 @@
-# Python 2
+# Add Homebrew Python to PATH
 
-PATH="$(brew --prefix python@2)/bin:$PATH"
-
-# Python 3
-
-PATH="~/Library/Python/3.7/bin:$PATH"
-
-# Python & Virtualenv
-
-export PYTHONPATH=${PYTHONPATH}:/usr/bin
-#export PIP_REQUIRE_VIRTUALENV=true
-export WORKON_HOME=~/.envs
-export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
-
-export PATH
-
-if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
-	source /usr/local/bin/virtualenvwrapper.sh
-fi
+PATH="$(brew --prefix python)/bin:$PATH"
