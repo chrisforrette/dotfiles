3c7a8aee1a55d72b58022ddb73ea4d47586f5a4d
chrisforrette <chris.forrette@splice.com>
2022-03-31 12:00:08 -0700

    Adding a separate directory for terraform stuff

---
+++ terraform/install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+brew list warrensbox/tap/tfswitch || brew install warrensbox/tap/tfswitch
---
+++ terraform/path.zsh
@@ -0,0 +1,11 @@
+# See: https://github.com/warrensbox/terraform-switcher#automation
+autoload -U add-zsh-hook
+load-tfswitch() {
+  local tfswitchrc_path=".tfswitchrc"
+
+  if [ -f "$tfswitchrc_path" ]; then
+    tfswitch
+  fi
+}
+add-zsh-hook chpwd load-tfswitch
+load-tfswitch
