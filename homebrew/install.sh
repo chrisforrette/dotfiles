a2fdb4833e79eaebea952c4f31c0b6cb9169cb9f
chrisforrette <chris@chrisforrette.com>
2026-02-25 21:50:55 -0800

    Fix dependency install logging, fix Safari flag setting errors, making sure Homebrew is available right after install

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -10,6 +10,13 @@ if test ! $(which brew)
 then
   echo "Installing Homebrew for you."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  # Make brew available in the current session after fresh install
+  if [ -f /opt/homebrew/bin/brew ]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  elif [ -f /usr/local/bin/brew ]; then
+    eval "$(/usr/local/bin/brew shellenv)"
+  fi
 fi
 
 brew update
